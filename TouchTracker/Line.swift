68a0cd7a7fbd9695b60e0c39cf8233fc5748373e
Anthony Demattos <ademattos@sub-mariner.highpoint.edu>
2019-04-10 13:14:26 -0400

    initial commit

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Anthony Demattos on 4/10/19.
+//  Copyright © 2019 Anthony Demattos. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
